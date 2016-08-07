a0cb79501eec4054eb67492de923d5c3667d3401
Alex Arslan <ararslan@comcast.net>
2016-08-07 13:38:32 -0700

    Actually properly prefix the build

--- deps/build.jl
+++ deps/build.jl
@@ -30,7 +30,7 @@ osf = library_dependency("openspecfun",
             ChangeDirectory(joinpath(srcdir(osf), "openspecfun-$vers"))
             FileRule(joinpath(libdir(osf), "libopenspecfun." * BinDeps.shlib_ext), @build_steps begin
                 CreateDirectory(libdir(osf))
-                `make install prefix=$(BinDeps.depsdir(osf))`
+                `make install prefix=$(BinDeps.usrdir(osf))`
             end)
         end
     end), osf)

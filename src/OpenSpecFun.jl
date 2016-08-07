529067b8d08fceb00a5d932c63c68b9f50887f2c
Alex Arslan <ararslan@comcast.net>
2016-08-06 20:20:31 -0700

    I forgot to export things

--- src/OpenSpecFun.jl
+++ src/OpenSpecFun.jl
@@ -11,6 +11,42 @@ else
     error("OpenSpecFun.jl not properly installed. Please run Pkg.build(\"OpenSpecFun\").")
 end
 
+export
+    # bessel
+    airy,
+    airyai,
+    airyaiprime,
+    airybi,
+    airybiprime,
+    airyprime,
+    airyx,
+    besselh,
+    besselhx,
+    besseli,
+    besselix,
+    besselj,
+    besselj0,
+    besselj1,
+    besseljx,
+    besselk,
+    besselkx,
+    bessely,
+    bessely0,
+    bessely1,
+    besselyx,
+    hankelh1,
+    hankelh1x,
+    hankelh2,
+    hankelh2x,
+
+    # erf
+    erf,
+    erfc,
+    erfcinv,
+    erfcx,
+    erfi,
+    erfinv
+
 for f in ["bessel", "erf"]
     include("$f.jl")
 end

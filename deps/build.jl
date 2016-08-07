176830c186c9ad340702de354253fc9f9f0eb338
Alex Arslan <ararslan@comcast.net>
2016-08-06 23:37:28 -0700

    I think I got it this time

--- deps/build.jl
+++ deps/build.jl
@@ -26,14 +26,11 @@ osf = library_dependency("openspecfun",
 
     provides(BuildProcess, (@build_steps begin
         GetSources(osf)
-        #CreateDirectory(joinpath(BinDeps.builddir(osf), "openspecfun"))
         @build_steps begin
-            # ChangeDirectory(joinpath(BinDeps.builddir(osf), "openspecfun"))
+            ChangeDirectory(joinpath(srcdir(osf), "openspecfun-$vers"))
             ChangeDirectory(srcdir(osf))
             FileRule(joinpath(libdir(osf), "libopenspecfun." * BinDeps.shlib_ext), @build_steps begin
                 CreateDirectory(libdir(osf))
-                `pwd`
-                `ls -R`
                 `make install`
             end)
         end
